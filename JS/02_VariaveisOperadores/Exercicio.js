41c5bf84b317fbf76211f9c13807cfd324d98e81
LawandaVitoria <Lawandavitoria90@gmail.com>
2022-04-27 21:42:59 -0300

    Exercicios

---
+++ JS/02_VariaveisOperadores/Exercicio.js
@@ -0,0 +1,6 @@
+const nome = 'Victória';
+const altura = 1.58;
+const peso = 58;
+const IMC =  peso / (altura * altura);
+console.log(IMC);
+console.log(`${nome} possui altura ${altura}m e peso ${peso}kg e seu IMC é ${IMC}`);
